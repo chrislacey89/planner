d85a57df5e042452ea6482051b74afba974dc14c
Christopher Lacey <christopherlacey@ChristophersMBP.home>
2024-04-13 22:58:59 -0400

    add data

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,12 +1,19 @@
 import { type MetaFunction } from '@remix-run/node'
-import { DataTable } from '#app/components/data-table/data-table.js'
-
+import { useLoaderData } from '@remix-run/react'
+import { columns } from '#app/components/data-table/columns'
+import tasks from '#app/components/data-table/data/tasks.json'
+import { DataTable } from '#app/components/data-table/data-table'
 export const meta: MetaFunction = () => [{ title: 'Todos' }]
+export async function loader() {
+	return tasks
+}
 
 export default function Index() {
+	const data = useLoaderData<typeof loader>()
+
 	return (
 		<main className="font-poppins grid h-full place-items-center">
-			<DataTable columns={[]} data={[]} />
+			<DataTable columns={columns} data={data} />
 		</main>
 	)
 }
