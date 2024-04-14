5b2448f8d8ae5a86ca32cc9685149721f8354a45
Christopher Lacey <christopherlacey@ChristophersMBP.home>
2024-04-13 22:44:43 -0400

    add data table

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,9 +1,12 @@
 import { type MetaFunction } from '@remix-run/node'
+import { DataTable } from '#app/components/data-table/data-table.js'
 
 export const meta: MetaFunction = () => [{ title: 'Todos' }]
 
 export default function Index() {
 	return (
-		<main className="font-poppins grid h-full place-items-center">yolo</main>
+		<main className="font-poppins grid h-full place-items-center">
+			<DataTable columns={[]} data={[]} />
+		</main>
 	)
 }
